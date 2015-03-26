6ce490069a2e3eb05fd3e5ea4c7d28a1721b15c2
saimushi <saimushi@10.1.108.77>
2015-03-26 12:16:03 +0900

    規定ファイル追加 コメント追加

---
+++ src/app/KiraKiraTimeCard/KiraKiraTimeCard/Class/MVC/View/AuthorizeEnabledView.swift
@@ -0,0 +1,11 @@
+//
+//  AuthorizeEnabledView.swift
+//  KiraKiraTimeCard
+//
+//  ログイン・ログアウト出来る状態になった時のView
+
+//  Created by saimushi on 2015/03/26.
+//  Copyright (c) 2015年 saimushi. All rights reserved.
+//
+
+import Foundation
---
+++ src/app/KiraKiraTimeCard/KiraKiraTimeCard/Class/MVC/View/UnauthCutInView.swift
@@ -0,0 +1,11 @@
+//
+//  UnauthCutInView.swift
+//  KiraKiraTimeCard
+//
+//  ログアウト(退勤)した時のView
+//  
+//  Created by saimushi on 2015/03/26.
+//  Copyright (c) 2015年 saimushi. All rights reserved.
+//
+
+import Foundation
