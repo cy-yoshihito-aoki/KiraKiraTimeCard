336be2cee64280959c03c4829ce9d2cdb263297d
saimushi <saimushi@10.1.108.77>
2015-03-28 04:12:19 +0900

    全クラスのプロトコルを実装 ビーコンエージェント以外の骨組み作成完了

--- src/app/KiraKiraTimeCard/KiraKiraTimeCard/Class/MVC/View/AuthCutInView.swift
+++ src/app/KiraKiraTimeCard/KiraKiraTimeCard/Class/MVC/View/AuthCutInView.swift
@@ -8,4 +8,19 @@
 //  Copyright (c) 2015年 saimushi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AuthCutInView :UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        var label :UILabel = UILabel(frame: CGRectMake(0, (frame.height-20)/2.0, frame.width, 20))
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "AuthCutInView"
+        addSubview(label)
+    }
+    
+    // これを実装しないとエラーになる
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
