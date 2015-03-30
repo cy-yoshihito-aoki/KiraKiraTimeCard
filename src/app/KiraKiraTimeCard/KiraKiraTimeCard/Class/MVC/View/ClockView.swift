4468811d5db285d31c1bc05d17a80fa7ec2db2ed
cy-maiko-takeda <maiko_takeda@digiq.co.jp>
2015-03-30 21:27:56 +0900

    edit clock

--- src/app/KiraKiraTimeCard/KiraKiraTimeCard/Class/MVC/View/ClockView.swift
+++ src/app/KiraKiraTimeCard/KiraKiraTimeCard/Class/MVC/View/ClockView.swift
@@ -13,9 +13,9 @@ class ClockView :UIView {
     private let secondLayer = CAShapeLayer()
     
     let dateLabel: UILabel = UILabel(frame: CGRectMake(30, 30, 400, 50))
-    let secondHandImageView: UIImageView = UIImageView(frame: CGRect(x: 30, y: 30, width: 2, height: 400))
-    let minuteHandImageView: UIImageView = UIImageView(frame: CGRect(x: 30, y: 30, width: 2, height: 400))
-    let hourHandImageView: UIImageView   = UIImageView(frame: CGRect(x: 30, y: 30, width: 5, height: 400))
+    let secondHandImageView: UIImageView = UIImageView(frame: CGRect(x: 30, y: 30, width: 2, height: 150))
+    let minuteHandImageView: UIImageView = UIImageView(frame: CGRect(x: 30, y: 30, width: 2, height: 100))
+    let hourHandImageView: UIImageView   = UIImageView(frame: CGRect(x: 30, y: 30, width: 5, height: 50))
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,19 +27,25 @@ class ClockView :UIView {
         secondHandImageView.backgroundColor = UIColor.orangeColor();
         minuteHandImageView.backgroundColor = UIColor.yellowColor();
         hourHandImageView.backgroundColor   = UIColor.blueColor();
-//        handImageView.backgroundColor   = UIColor.magentaColor();
-        secondHandImageView.layer.position = CGPoint(x: self.bounds.width/2, y: 200)
+        
+        secondHandImageView.frame = CGRect(x: (self.bounds.width - 2)/2, y: 30, width: 2, height: 300)
+        minuteHandImageView.frame = CGRect(x: (self.bounds.width - 2)/2, y: 30, width: 2, height: 200)
+        hourHandImageView.frame   = CGRect(x: (self.bounds.width - 5)/2, y: 30, width: 5, height: 100)
+        
+//        secondHandImageView.layer.position = CGPoint(x: self.bounds.width/2, y: 150)
+//        minuteHandImageView.layer.position = CGPoint(x: self.bounds.width/2, y: 100)
+//        hourHandImageView.layer.position   = CGPoint(x: self.bounds.width/2, y: 50)
+        
         addSubview(secondHandImageView)
-//        addSubview(handImageView)
-//        addSubview(minuteHandImageView)
-//        addSubview(hourHandImageView)
+        addSubview(minuteHandImageView)
+        addSubview(hourHandImageView)
         
         addSubview(dateLabel)
         
-        // 初期化
-        secondHandImageView.transform = CGAffineTransformMakeRotation(0)
-        minuteHandImageView.transform = CGAffineTransformMakeRotation(0)
-        hourHandImageView.transform   = CGAffineTransformMakeRotation(0)
+//        // 初期化
+//        secondHandImageView.transform = CGAffineTransformMakeRotation(0)
+//        minuteHandImageView.transform = CGAffineTransformMakeRotation(0)
+//        hourHandImageView.transform   = CGAffineTransformMakeRotation(0)
         
         // タイマー（1秒毎）
         var timer = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector:"reloadClock", userInfo:nil, repeats:true)
@@ -98,7 +104,7 @@ class ClockView :UIView {
             NSCalendarUnit.CalendarUnitWeekday,
             fromDate: myDate)
         
-        //        let weekdayStrings: Array = ["nil","日","月","火","水","木","金","土","日"]
+//        let weekdayStrings: Array = ["nil","日","月","火","水","木","金","土","日"]
         
         //        println("year: \(myComponetns.year)")
         //        println("month: \(myComponetns.month)")
@@ -116,71 +122,13 @@ class ClockView :UIView {
         myStr += "\(myComponetns.minute)分"
         myStr += "\(myComponetns.second)秒"
         dateLabel.text = myStr
-        
-        
-        //        // 時、分、秒を取得
-        //        NSCalendar *calendar = [NSCalendar currentCalendar];
-        //        NSDateComponents *dateComps = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:date];
-        //
-        //        // 回転角度を計算
-        //        NSInteger minute = 60 * dateComps.hour + dateComps.minute;
-        //        CGAffineTransform rotateHour = CGAffineTransformMakeRotation( 2 * M_PI * minute / (60 * 12));
-        //        CGAffineTransform rotateMin = CGAffineTransformMakeRotation( 2 * M_PI * dateComps.minute / 60);
-        //        CGAffineTransform rotateSec = CGAffineTransformMakeRotation( 2 * M_PI * dateComps.second / 60);
-        //
-        //        // アニメーションをつけて実行
-        //        [UIView animateWithDuration:0.1f
-        //            animations: ^{
-        //            self.hourImageView.transform = rotateHour;
-        //            self.minImageView.transform = rotateMin;
-        //            self.secImageView.transform = rotateSec;
-        //            } ];
-        //    }
-        //
-        
 
         // radianで回転角度を指定
         let minute: CGFloat = CGFloat(60 * myComponetns.hour + myComponetns.minute)
-        
-        println("minute: \(minute)")
-        
-        
-        let aa: CGFloat = CGFloat(myComponetns.second / 60)
-        let bb: CGFloat = CGFloat(myComponetns.minute / 60)
 
-//        let angleSec:Double = 2 * M_PI * Double(minute / (60 * 12))
-//        let angleMin:CGFloat = 2 * CGFloat(M_PI) * CGFloat(myComponetns.minute/60)
-//        let angleHor:CGFloat = 2 * CGFloat(M_PI) * CGFloat(myComponetns.second/60)
-        
-//        let angleSec:CGFloat = 2 * M_PI * dateComps.second / 60
-//        let angleMin:CGFloat = 2 * M_PI * dateComps.minute / 60
-//        let angleHor:CGFloat = 2 * M_PI * minute / (60 * 12)
-        
-        
-//        // 見本
-//        CGAffineTransform rotateSec = CGAffineTransformMakeRotation( 2 * M_PI * dateComps.second / 60);
-//        CGAffineTransform rotateMin = CGAffineTransformMakeRotation( 2 * M_PI * dateComps.minute / 60);
-//        CGAffineTransform rotateHour = CGAffineTransformMakeRotation( 2 * M_PI * minute / (60 * 12));
-        
-//        float radian = 45 * M_PI / 180;
-  
-        let angleSec:CGFloat = 2 * CGFloat(M_PI) * aa
-        let angleMin:CGFloat = 2 * CGFloat(M_PI) * CGFloat(myComponetns.minute/60);
+        let angleSec:CGFloat = 2 * CGFloat(M_PI) * CGFloat(myComponetns.second) / 60.0
+        let angleMin:CGFloat = 2 * CGFloat(M_PI) * CGFloat(myComponetns.minute) / 60.0
         let angleHor:CGFloat = 2 * CGFloat(M_PI) * minute/(60 * 12);
-        
-        
-//        let rotateSec:CGAffineTransform  = CGAffineTransformMakeRotation( 2 * M_PI * myComponetns.second) / 60)
-//        let rotateMin:CGAffineTransform  = CGAffineTransformMakeRotation( 2 * M_PI * myComponetns.minute / 60)
-//        let rotateHour:CGAffineTransform = CGAffineTransformMakeRotation( 2 * M_PI * minute / (60 * 12))
-        
-        
-        
-        
-        
-//        println("angleHor: \(angleHor)")
-//        println("angleMin: \(angleMin)")
-//        println("angleSec: \(angleSec)")
-        
 
         // アニメーションの秒数を設定
         UIView.animateWithDuration(1.0,
@@ -194,51 +142,6 @@ class ClockView :UIView {
             },
             completion: { (Bool) -> Void in
         })
-        
-        
-        
-        
-        
-        let now = NSDate() // 現在日時の取得
-        
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.locale = NSLocale(localeIdentifier: "en_US") // ロケールの設定
-        dateFormatter.dateFormat = "ss" // 日付フォーマットの設定
-        var sec = dateFormatter.stringFromDate(now) // 現在時刻の取得
-        
-//        // 秒針の設定(詳細を覚える必要は無いです)
-//        var angle: Double = Double(sec.toInt()!)    // 数値に変換
-//        var mRad = -90 * M_PI / 180;    // ベースの針の長さ
-//        var pi:Double = M_PI * 2    // 直径
-//        var x = 50 + 50.0 * cos(angle * pi / 60.0 + mRad)   // x座標
-//        var y = 50 + 50.0 * sin(angle * pi / 60.0 + mRad)   // y座標
-//        var x_float:CGFloat = CGFloat(x)    // 表示用の型に変換
-//        var y_float:CGFloat = CGFloat(y)    // 表示用の型に変換
-//        
-//        // コンテキストを設定
-//        UIGraphicsBeginImageContextWithOptions(CGSizeMake(100, 100), false, 0)
-//        let context = UIGraphicsGetCurrentContext()
-//        
-//        // 直線
-//        CGContextSetLineWidth(context, 1.0);   // 線の太さ
-//        CGContextSetRGBStrokeColor(context, 1, 1, 1, 1) // 線の色(白)
-//        CGContextMoveToPoint(context, 50, 50);  // 始点
-//        CGContextAddLineToPoint(context, x_float, y_float);  // 終点
-//        CGContextStrokePath(context);
-//        
-//        // 描画した画像をスクリーンショットとして保存
-//        let secondHandImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        
-//        // 描画
-//        // アニメーションの秒数を設定
-//        UIView.animateWithDuration(1.0,
-//            animations: { () -> Void in
-//                // 1秒毎に秒針を更新
-//                self.secondHandImageView.image = secondHandImage
-//            },
-//            completion: { (Bool) -> Void in
-//        })
     }
     
     func update(displayLink: CADisplayLink) {
